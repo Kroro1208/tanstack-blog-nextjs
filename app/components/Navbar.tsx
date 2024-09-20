f75cee1b43cf716cea3e8e10376913421b940534
Kroro1208 <yamagata_7580@yahoo.co.jp>
2024-09-20 14:14:09 +0900

    環境構築してdaisyUIインストールしてNabvarで反映させた

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,40 @@
+import Link from "next/link";
+
+const Navbar: React.FC = () => {
+  return (
+    <div className="navbar bg-base-100">
+      <div className="flex-1">
+        <Link href="/">
+            <p  className="btn btn-ghost text-xl">
+            daisyUI
+            </p>
+        </Link>
+      </div>
+      <div className="flex-none">
+        <button
+          type="button"
+          className="btn btn-square btn-ghost"
+          aria-label="Menu"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            className="inline-block h-5 w-5 stroke-current"
+            aria-hidden="true"
+          >
+            <title>Menu Icon</title>
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
+            />
+          </svg>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
